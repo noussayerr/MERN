3cefa64db0bfa191cc2e722268400a2a4826be64
rahal <rahalnoussayerr@gmail.com>
2024-01-12 02:58:33 +0100

    add

--- react/functional_components/hook_form/hook_form/src/App.js
+++ react/functional_components/hook_form/hook_form/src/App.js
@@ -2,7 +2,7 @@ import './App.css';
 import Form from './components/Form'
 function App() {
   return (
-    <div className="App">
+    <div className="">
       <Form />
     </div>
   );
--- react/functional_components/hook_form/hook_form/src/components/Form.jsx
+++ react/functional_components/hook_form/hook_form/src/components/Form.jsx
@@ -1,44 +1,56 @@
 import React, { useState } from  'react';
     
 const UserForm = (props) => {
-    const [username, setUsername] = useState("");
+    const [firstname, setfirstname] = useState("");
+    const [lastname, setlastname] = useState("");
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");  
-    
+    const [confirmPassword, setconfirmPassword] = useState("");  
     const createUser = (e) => {
-        // we must prevent the default refresh of the browser to keep our state from being reset
+
         e.preventDefault();
-    
-        // create a javascript object to hold all of the values
-        const newUser = { 
-            username: username, 
-            email: email, 
-            password: password 
-        };
+        const newUser = { firstname,lastname, email, password ,confirmPassword};
         console.log("Welcome", newUser);
+    	setfirstname("");
+        setlastname("")
+    	setEmail("");
+    	setPassword("");
+        setconfirmPassword("");
     };
     
     return(
-        <div>
+        <div className='App'>
         <form onSubmit={ createUser }>
-            <div>
-                <label>Username: </label> 
-                <input type="text" onChange={ (e) => setUsername(e.target.value) } />
+            <div className='inputxlabel'>
+                <label>First Name :</label>
+                <input type='text' value={firstname} onChange={(e)=>setfirstname(e.target.value)} />
             </div>
-            <div>
+            <div className='inputxlabel'>
+                <label>Last Name: </label> 
+                <input type="text"value={lastname} onChange={ (e) => setlastname(e.target.value) } />
+            </div>
+            <div className='inputxlabel'>
                 <label>Email Address: </label> 
-                <input type="text" onChange={ (e) => setEmail(e.target.value) } />
+                <input type="text" value={email} onChange={ (e) => setEmail(e.target.value) } />
             </div>
-            <div>
+            <div className='inputxlabel'>
                 <label>Password: </label>
-                <input type="text" onChange={ (e) => setPassword(e.target.value) } />
+                <input type="password" value={password} onChange={ (e) => setPassword(e.target.value) } />
             </div>
-            <input type="submit" value="Create User" />
-            <h3>Your form data</h3>
-            <p>First Name : {username}</p>
-            <p>Last Name :</p>
-            <p>email: </p>
+            <div className='inputxlabel'>
+                <label>confirm Password: </label>
+                <input type="password" value={confirmPassword} onChange={ (e) => setconfirmPassword(e.target.value) } />
+            </div>
+            <input className='btn' type="submit" value="Create User" />
         </form>
+        <div className='form_data'>
+                <h3>Your Form Data</h3>
+                <p><label>First Name :</label> {firstname}</p>
+                <p><label>Last Name :</label>{lastname}</p>
+                <p><label>Email:</label>{email}</p>
+                <p><label>Password :</label>{password}</p>
+                <p><label>confirm password :</label>{confirmPassword}</p>
+            </div>
         </div>
     );
 };
