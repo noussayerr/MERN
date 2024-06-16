a0c57ac9289bc8b200b836bf169ea3e04f844fd6
noussayerr <noussayer.rahal.83@gmail.com>
2024-06-16 23:57:42 +0100

    add

---
+++ advanced_mern/class components/big_inversion/src/components/personne.jsx
@@ -0,0 +1,17 @@
+import React, { Component } from "react";
+
+class PersonCard extends Component {
+  render() {
+    const { firstName, lastName, age, hair } = this.props;
+    return (
+      <div className="container">
+        <h2>
+          {lastName}, {firstName}
+        </h2>
+        <p>Age: {age}</p>
+        <p>Hair Color: {hair}</p>
+      </div>
+    );
+  }
+}
+export default PersonCard;
